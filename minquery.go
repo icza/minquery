14b08e3fb8ae92d81171ee076623b498792b919f
Andras Belicza <iczaaa@gmail.com>
2016-11-16 14:34:04 +0100

    Added implementation.

--- minquery.go
+++ minquery.go
@@ -2,6 +2,13 @@
 // MongoDB pagination (cursors to continue listing documents where we left off).
 package minquery
 
+import (
+	"encoding/base64"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
 // MinQuery is an mgo-like Query that supports cursors to continue listing documents
 // where we left off. If a cursor is set, it specifies the last index entry
 // that was already returned, and result documents will be listed after this.
@@ -24,8 +31,215 @@ type MinQuery interface {
 	// is specified, All() will fail and return the error.
 	Cursor(c string) MinQuery
 
+	// CursorCoded sets the CursorCodec to be used to parse and to create cursors.
+	CursorCodec(cc CursorCodec) MinQuery
+
 	// All retrieves all documents from the result set into the provided slice.
 	// cursFields lists the fields (in order) to be used to generate
 	// the returned cursor.
 	All(result interface{}, cursorFields ...string) (cursor string, err error)
+}
+
+// CursorCodec represents a symmetric pair of functions that can be used to
+// convert cursor data of type bson.D to a string and vice versa.
+type CursorCodec interface {
+	// CreateCursor returns a cursor string from the specified fields.
+	CreateCursor(cursorData bson.D) (string, error)
+
+	// ParseCursor parses the cursor string and returns the cursor data.
+	ParseCursor(c string) (cursorData bson.D, err error)
+}
+
+// minQuery is the MinQuery implementation.
+type minQuery struct {
+	// db is the mgo Database to use
+	db *mgo.Database
+
+	// Name of the collection
+	coll string
+
+	// filter document (query)
+	filter interface{}
+
+	// sort document
+	sort bson.D
+
+	// projection document (to retrieve only selected fields)
+	projection interface{}
+
+	// limit is the max number of results
+	limit int
+
+	// Cursor, need to store and supply it if query returns no results
+	cursor string
+
+	// cursorCodec to be used to parse and to create cursors
+	cursorCodec CursorCodec
+
+	// cursorErr contains an error if an invalid cursor is supplied
+	cursorErr error
+
+	// min specifies the last index entry
+	min bson.D
+}
+
+// New returns a new MinQuery.
+func New(db *mgo.Database, coll string, query interface{}) MinQuery {
+	return &minQuery{
+		db:     db,
+		coll:   coll,
+		filter: query,
+	}
+}
+
+// Sort implements MinQuery.Sort().
+func (mq *minQuery) Sort(fields ...string) MinQuery {
+	mq.sort = make(bson.D, 0, len(fields))
+	for _, field := range fields {
+		if field == "" {
+			continue
+		}
+		n := 1
+		if field[0] == '+' {
+			field = field[1:]
+		} else if field[0] == '-' {
+			n, field = -1, field[1:]
+		}
+		mq.sort = append(mq.sort, bson.DocElem{Name: field, Value: n})
+	}
+	return mq
+}
+
+// Select implements MinQuery.Select().
+func (mq *minQuery) Select(selector interface{}) MinQuery {
+	mq.projection = selector
+	return mq
+}
+
+// Limit implements MinQuery.Limit().
+func (mq *minQuery) Limit(n int) MinQuery {
+	mq.limit = n
+	return mq
+}
+
+// Cursor implements MinQuery.Cursor().
+func (mq *minQuery) Cursor(c string) MinQuery {
+	mq.cursor = c
+	if c != "" {
+		mq.min, mq.cursorErr = mq.cursorCodec.ParseCursor(c)
+	} else {
+		mq.min, mq.cursorErr = nil, nil
+	}
+	return mq
+}
+
+// CursorCodec implements MinQuery.CursorCodec().
+func (mq *minQuery) CursorCodec(cc CursorCodec) MinQuery {
+	mq.cursorCodec = cc
+	return mq
+}
+
+// All implements MinQuery.All().
+func (mq *minQuery) All(result interface{}, cursorFields ...string) (cursor string, err error) {
+	if mq.cursorErr != nil {
+		return "", mq.cursorErr
+	}
+
+	// Mongodb "find" reference:
+	// https://docs.mongodb.com/manual/reference/command/find/
+
+	cmd := bson.D{
+		{Name: "find", Value: mq.coll},
+		{Name: "limit", Value: mq.limit},
+		{Name: "batchSize", Value: mq.limit},
+		{Name: "singleBatch", Value: true},
+	}
+	if mq.filter != nil {
+		cmd = append(cmd, bson.DocElem{Name: "filter", Value: mq.filter})
+	}
+	if mq.sort != nil {
+		cmd = append(cmd, bson.DocElem{Name: "sort", Value: mq.sort})
+	}
+	if mq.projection != nil {
+		cmd = append(cmd, bson.DocElem{Name: "projection", Value: mq.projection})
+	}
+	if mq.min != nil {
+		// min is inclusive, skip the first (which is the previous last)
+		cmd = append(cmd,
+			bson.DocElem{Name: "skip", Value: 1},
+			bson.DocElem{Name: "min", Value: mq.min},
+		)
+	}
+
+	var res struct {
+		OK       int `bson:"ok"`
+		WaitedMS int `bson:"waitedMS"`
+		Cursor   struct {
+			ID         interface{} `bson:"id"`
+			NS         string      `bson:"ns"`
+			FirstBatch []bson.Raw  `bson:"firstBatch"`
+		} `bson:"cursor"`
+	}
+
+	if err = mq.db.Run(cmd, &res); err != nil {
+		return
+	}
+
+	firstBatch := res.Cursor.FirstBatch
+	if len(firstBatch) > 0 {
+		if len(cursorFields) > 0 {
+			// create cursor from the last document
+			var doc bson.M
+			if err = firstBatch[len(firstBatch)-1].Unmarshal(&doc); err != nil {
+				return
+			}
+			cursorData := make(bson.D, len(cursorFields))
+			for i, cf := range cursorFields {
+				cursorData[i] = bson.DocElem{Name: cf, Value: doc[cf]}
+			}
+			cursor, err = mq.cursorCodec.CreateCursor(cursorData)
+			if err != nil {
+				return
+			}
+		}
+	} else {
+		// No more results. Use the same cursor that was used for the query.
+		// It's possible that the last doc was returned previously, and there
+		// are no more.
+		cursor = mq.cursor
+	}
+
+	// Unmarshal results (FirstBatch) into the user-provided value:
+	err = mq.db.C(mq.coll).NewIter(nil, firstBatch, 0, nil).All(result)
+	return
+}
+
+// DefaultCursorCodec is the default CursorCodec value that is used if none
+// is specified. The default implementation produces web-safe cursor strings.
+var DefaultCursorCodec cursorCodec
+
+// cursorCodec is a default implementation of CursorCodec which produces
+// web-safe cursor strings by first marshalling the cursor data using
+// bson.Marshal(), then using base64.RawURLEncoding.
+type cursorCodec struct{}
+
+// CreateCursor implements CursorCodec.CreateCursor().
+// The returned cursor string is web-safe, and so it's safe to include
+// in URL queries without escaping.
+func (cc cursorCodec) CreateCursor(cursorData bson.D) (string, error) {
+	// bson.Marshal() never returns error, so I skip a check and early return
+	// (but I do return the error if it would even happen)
+	data, err := bson.Marshal(cursorData)
+	return base64.RawURLEncoding.EncodeToString(data), err
+}
+
+// ParseCursor implements CursorCodec.ParseCursor().
+func (cc cursorCodec) ParseCursor(c string) (cursorData bson.D, err error) {
+	var data []byte
+	if data, err = base64.RawURLEncoding.DecodeString(c); err != nil {
+		return
+	}
+
+	err = bson.Unmarshal(data, &cursorData)
+	return
 }
